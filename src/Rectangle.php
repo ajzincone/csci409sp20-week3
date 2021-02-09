ad067b0b357cbbc500e7cc3e7ea1f20cebaaaa94
Anthony Zincone <ajzincone@coastal.edu>
2021-02-08 22:31:48 -0500

    Created Rectangle class.

---
+++ src/Rectangle.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * Class: csci303fa20
+ * User: ajzincone
+ * Date: 2/8/2021
+ * Time: 10:16 PM
+ */
+ 
+  class Rectangle extends Shapes
+  {
+     const SHAPE_TYPE = 2;
+
+
+     public function getFullDescription()
+     {
+         return "Rectangle<#$this->id>: $this->name - $this->length x $this->width";
+     }
+
+
+
+
+
+
+
+  }
+
+
