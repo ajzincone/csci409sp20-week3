458518efd4829525b142697e3a0c13b70b0cd4ed
Anthony Zincone <ajzincone@coastal.edu>
2021-02-08 22:12:36 -0500

    Created Shapes class.

---
+++ src/Shapes.php
@@ -0,0 +1,62 @@
+<?php
+
+/**
+ * Class: csci409fa20
+ * User: ajzincone
+ * Date: 2/8/2021
+ * Time: 8:07 PM
+ */
+ 
+  class Shapes
+  {
+      const SHAPE_TYPE = 1;
+      public $name = "";
+      protected $length = "";
+      protected $width = "";
+      private $id = "";
+
+      function __construct($length, $width)
+      {
+          $this->name = "";
+          $this->length = $length;
+          $this->width = $width;
+          $id = uniqid();
+      }
+
+      function getName()
+      {
+          return $this->name;
+      }
+
+      function setName($name)
+      {
+          $this->name = $name;
+      }
+
+      function getId()
+      {
+          return $this->id;
+      }
+
+      public function area()
+      {
+          return $this->length * $this->width;
+      }
+
+      public static function getTypeDescription()
+      {
+          return "Type: " . static::SHAPE_TYPE;
+      }
+      public function getFullDescription()
+      {
+          return "Shape<#$this->id>: $this->name - $this->length x $this->width";
+      }
+
+
+
+
+
+
+
+
+  }
